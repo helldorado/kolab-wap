e1422bfb413fee8d17cb75181f87815cae1122c5
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2011-12-27 11:50:14 +0100

    Do not enforce / require the session.use_cookies setting to be set to either true or false

--- lib/functions.php
+++ lib/functions.php
@@ -8,13 +8,12 @@ define('KADM_VERSION', '0.1');
 define('KADM_CHARSET', 'utf-8');
 define('INSTALL_PATH', dirname(__FILE__));
 
-
 // Check critical PHP settings here.
 $crit_opts = array(
     'mbstring.func_overload' => 0,
     'magic_quotes_runtime' => 0,
-    'session.use_cookies' => 1,
 );
+
 foreach ($crit_opts as $optname => $optval) {
     if ($optval != ini_get($optname)) {
         die("ERROR: Wrong '$optname' option value!");
