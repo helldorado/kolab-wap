ee4c999c13c60588fab7a44be6e680e94cfe3393
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2011-12-27 13:11:07 +0100

    Modify the group list page to use the now implemented groups.list GET method service handler

--- lib/client/kolab_client_task_group.php
+++ lib/client/kolab_client_task_group.php
@@ -12,6 +12,8 @@ class kolab_client_task_group extends kolab_client_task
     {
         $this->output->set_object('content', '');
         $this->output->set_object('task_navigation', $this->menu());
+
+        $this->action_list();
     }
 
     public function action_list()
@@ -19,30 +21,27 @@ class kolab_client_task_group extends kolab_client_task
 //        $content = 'test output from users.list';
         $result = $this->api->get('groups.list');
         $result = (array) $result->get();
-/*
         foreach ($result as $idx => $item) {
-            if (!is_array($item) || empty($item['uid'])) {
+            if (!is_array($item) || empty($item['cn'])) {
                 unset($result[$idx]);
                 continue;
             }
-            $result[$idx] = sprintf('<li><a href="#" onclick="kadm.command(\'user.info\', \'%s\')">%s</a></li>',
-                $idx, $item['uid']);
+            $result[$idx] = sprintf('<li><a href="#" onclick="kadm.command(\'group.info\', \'%s\')">%s</a></li>',
+                $idx, $item['cn']);
         }
 
-        $result = '<ul id="userlist">' . implode("\n", $result) . '</ul>';
-*/
+        $result = '<ul id="grouplist">' . implode("\n", $result) . '</ul>';
         $this->output->set_object('content', $result);
     }
 
     public function action_info()
     {
-/*
-        $id     = $this->get_input('id', 'POST');
-        $result = $this->api->get('user.info', array('user' => $id));
 
-        $user = $result->get($id);
-        $this->output->set_object('content', print_r($user, true));
-*/
+        $id     = $this->get_input('id', 'POST');
+        $result = $this->api->get('group.info', array('group' => $id));
+
+        $group = $result->get($id);
+        $this->output->set_object('content', print_r($group, true));
     }
 
     public function group_add()
