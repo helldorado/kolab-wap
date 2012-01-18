6a407e558d1c84e95584c704c3e2e5d4dfbdc415
Aleksander Machniak <machniak@kolabsys.com>
2012-01-18 15:22:08 +0100

    Users list pagination support Lists return 'list' and 'count' items CS fixes

--- lib/api/kolab_domains_actions.php
+++ lib/api/kolab_domains_actions.php
@@ -1,23 +1,27 @@
 <?php
 
-    /**
-     *
-     */
-    class kolab_domains_actions extends kolab_api_service
+/**
+ *
+ */
+class kolab_domains_actions extends kolab_api_service
+{
+    public function capabilities($domain)
     {
-        public function capabilities($domain)
-        {
-            return array(
-                    'list' => 'r',
-//                     'search' => 'r',
-                );
-        }
-
-        public function domains_list($get, $post) {
-            $auth = Auth::get_instance();
-            $domains = $auth->list_domains();
-            return $domains;
-        }
+        return array(
+            'list' => 'r',
+//             'search' => 'r',
+        );
     }
 
-?>
+    public function domains_list($get, $post) {
+        $auth = Auth::get_instance();
+  
+        $domains = $auth->list_domains();
+        $count   = count($domains);
+  
+        return array(
+            'list'  => $domains,
+            'count' => $count,
+        );
+    }
+}
