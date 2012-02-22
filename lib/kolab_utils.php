395cd9af60479e5d3339a980cdfc08a202b13c56
Aleksander Machniak <alec@alec.pl>
2012-02-22 13:13:00 +0100

    Added license headers and LICENSE file

--- lib/kolab_utils.php
+++ lib/kolab_utils.php
@@ -1,4 +1,27 @@
 <?php
+/*
+ +--------------------------------------------------------------------------+
+ | This file is part of the Kolab Web Admin Panel                           |
+ |                                                                          |
+ | Copyright (C) 2011-2012, Kolab Systems AG                                |
+ |                                                                          |
+ | This program is free software: you can redistribute it and/or modify     |
+ | it under the terms of the GNU Affero General Public License as published |
+ | by the Free Software Foundation, either version 3 of the License, or     |
+ | (at your option) any later version.                                      |
+ |                                                                          |
+ | This program is distributed in the hope that it will be useful,          |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             |
+ | GNU Affero General Public License for more details.                      |
+ |                                                                          |
+ | You should have received a copy of the GNU Affero General Public License |
+ | along with this program. If not, see <http://www.gnu.org/licenses/>      |
+ +--------------------------------------------------------------------------+
+ | Author: Aleksander Machniak <machniak@kolabsys.com>                      |
+ | Author: Jeroen van Meeuwen <vanmeeuwen@kolabsys.com>                     |
+ +--------------------------------------------------------------------------+
+*/
 
 class kolab_utils
 {
