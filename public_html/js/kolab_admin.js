helldorado/kolab-wap

commit 63c1224c05a49b2e4382c96478ed7b3e20002d86

Be compatible with lists that only have values

public_html/js/kolab_admin.js
@@ -763,7 +763,7 @@ function kolab_admin()
       for (i in list) {
         elem = $('<input>');
         elem.attr({
-          value: list[i],
+          value: list[i] ? list[i] : i,
           disabled: disabled,
           readonly: readonly,
           name: form_element.name + '[' + (j++) + ']'
@@ -792,7 +792,7 @@ function kolab_admin()
       ac_value = []
       for (i in list) {
         elem = this.form_list_element(form_element.form, {
-          value: list[i],
+          value: list[i] ? list[i] : i,
           key: i,
           maxlength: maxlength,
           autocomplete: autocomplete,
@@ -800,7 +800,7 @@ function kolab_admin()
         }, j++);
 
         if (autocomplete)
-            ac_value.push(list[i]);
+            ac_value.push(list[i] ? list[i] : i);
 
         elem.appendTo(area);
       }

commit 5a3ce379da164a44d5cbc9d7ef5da0e31a942325

Fix "0" empty in list widgets (broken in 63c1224c05a49b2e4382c96478ed7b3e20002d86)

public_html/js/kolab_admin.js
@@ -750,10 +750,11 @@ function kolab_admin()
 
     if (!list) {
       if (form_element.value)
-        list = $.extend({}, form_element.value.split("\n"));
+        list = form_element.value.split("\n");
       else if (!autocomplete || disabled || readonly)
         list = {0: ''};
     }
+    list = $.extend({}, list);
 
     // Create simple list for readonly/disabled
     if (disabled || readonly) {
@@ -763,7 +764,7 @@ function kolab_admin()
       for (i in list) {
         elem = $('<input>');
         elem.attr({
-          value: list[i] ? list[i] : i,
+          value: list[i],
           disabled: disabled,
           readonly: readonly,
           name: form_element.name + '[' + (j++) + ']'
@@ -792,7 +793,7 @@ function kolab_admin()
       ac_value = []
       for (i in list) {
         elem = this.form_list_element(form_element.form, {
-          value: list[i] ? list[i] : i,
+          value: list[i],
           key: i,
           maxlength: maxlength,
           autocomplete: autocomplete,
@@ -800,7 +801,7 @@ function kolab_admin()
         }, j++);
 
         if (autocomplete)
-            ac_value.push(list[i] ? list[i] : i);
+            ac_value.push(list[i]);
 
         elem.appendTo(area);
       }