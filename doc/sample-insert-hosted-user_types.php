bc730a0c082eb6204edbab20bd46e241c37b4a77
Torsten Grote <grote@kolabsys.com>
2012-07-09 15:02:45 +0200

    new user_types table for hosted case

---
+++ doc/sample-insert-hosted-user_types.php
@@ -0,0 +1,121 @@
+#!/usr/bin/php
+<?php
+
+    if (isset($_SERVER["REQUEST_METHOD"]) && !empty($SERVER["REQUEST_METHOD"])) {
+        die("Not intended for execution through the webserver, sorry!");
+    }
+
+    require_once("lib/functions.php");
+
+    $db   = SQL::get_instance();
+
+    $result = $db->query("TRUNCATE `user_types`");
+
+    $attributes = Array(
+
+            /*
+             *
+             * The form fields for which the values can be
+             * generated automatically, using the existing
+             * values of form_fields
+             */
+            "auto_form_fields" => Array(
+                    /*
+                     *
+                     * The 'cn' attribute is required for
+                     * the LDAP objectclasses we use, but
+                     * can be composed from a 'givenname'
+                     * and 'sn' attribute form_field (of
+                     * which 'sn' is also a required
+                     * attribute.
+                     */
+                    "cn" => Array(
+                            "data" => Array(
+                                    "givenname",
+                                    "sn",
+                                ),
+                        ),
+                    /*
+                     *
+                     * The 'mail' attribute is supposed to 
+                     * contain the email address this user
+                     * will use for this environment, and
+                     * is (supposed?) to match the 'uid'
+                     * for the user account.
+                     *
+                     * Disable this auto_form_field if
+                     * the API is not capable of making
+                     * a 'uid' become a 'uid'@'domain',
+                     * where 'domain' is not a valid
+                     * LDAP attribute for a user entry.
+                     */
+                    "mail" => Array(
+                            "data" => Array(
+                                    "uid",
+                                ),
+                        ),
+                ),
+            "form_fields" => Array(
+                    "givenname" => Array(),
+                    "kolabdelegate" => Array(
+                            "type" => "list",
+                            "autocomplete" => true,
+                            "optional" => true,
+                        ),
+                    "kolabinvitationpolicy" => Array(
+                            "type" => "select",
+                            "values" => Array(
+                                    "",
+                                    "ACT_MANUAL",
+                                    "ACT_REJECT",
+                                ),
+                            "optional" => true,
+                        ),
+                    "kolaballowsmtprecipient" => Array(
+                            "type" => "list",
+                            "optional" => true,
+                        ),
+                    "kolaballowsmtpsender" => Array(
+                            "type" => "list",
+                            "optional" => true,
+                        ),
+                    /*
+                     *
+                     * The 'mailalternateaddress' is supposed
+                     * to contain the original email address
+                     * for the user.
+                     */
+                    "mailalternateaddress" => Array(
+                            "type" => "list",
+                            "optional" => true,
+                        ),
+                    "mailquota" => Array(
+                            "optional" => true,
+                        ),
+                    "nsroledn" => Array(
+                            "type" => "list",
+                            "autocomplete" => true,
+                            "optional" => true
+                        ),
+                    "sn" => Array(),
+                    "uid" => Array(),
+                    "userpassword" => Array(
+                            "optional" => true,
+                        ),
+                ),
+            "fields" => Array(
+                    "objectclass" => Array(
+                            "top",
+                            "inetorgperson",
+                            "kolabinetorgperson",
+                            "mailrecipient",
+                            "organizationalperson",
+                            "person",
+                        ),
+                ),
+        );
+
+    $result = $db->query("INSERT INTO `user_types` (`key`, `name`, `description`, `attributes`) " .
+                "VALUES ('personal','Personal', 'A user with a personal hosted plan'," .
+                "'" . json_encode($attributes) . "')");
+?>
