f736b8d5d8d06a7106859002c7d8d822cc90cd4d
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2012-09-28 13:55:31 +0200

    Sort and slice the array returned as part of form_value.list_options_member(), as it includes users and groups. (#1010)

--- lib/api/kolab_api_service_form_value.php
+++ lib/api/kolab_api_service_form_value.php
@@ -1003,7 +1003,6 @@ class kolab_api_service_form_value extends kolab_api_service
         $data['attributes'] = array('displayname', 'cn', 'mail');
 
         $service = $this->controller->get_service('users');
-
         $result  = $service->users_list(null, $data);
 
         $list    = $result['list'];
@@ -1027,6 +1026,13 @@ class kolab_api_service_form_value extends kolab_api_service
             if (!empty($value['mail'])) {
                 $list[$idx] .= ' <' . $value['mail'] . '>';
             }
+        }
+
+        // Sort and slice
+        asort($list);
+
+        if (!empty($data['page_size'])) {
+            $list = array_slice($list, 0, $data['page_size']);
         }
 
         return $list;
