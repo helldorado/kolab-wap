c547543b2264387e515159743cb6750c0fb4a628
Aleksander Machniak <alec@alec.pl>
2012-10-05 12:07:31 +0200

    Fix PHP warning when using array_combine() with an empty array

--- lib/client/kolab_client_task_settings.php
+++ lib/client/kolab_client_task_settings.php
@@ -820,7 +820,10 @@ class kolab_client_task_settings extends kolab_client_task
         if (!empty($response['list'])) {
             // remove objectClass
             $attributes = array_diff($response['list'], array('objectClass'));
-            $attributes = array_combine(array_map('strtolower', $attributes), $attributes);
+            $attributes = array_map('strtolower', $attributes);
+            if (count($attributes)) {
+                $attributes = array_combine($attributes, $attributes);
+            }
         }
 
         $this->output->set_env('attributes', $attributes);
